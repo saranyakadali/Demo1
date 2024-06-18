65136ad9fd3b53c55d11955210c8166b05c11285
Sudheer Donaboina <sdonaboina@caesars.com>
2024-06-18 13:58:19 -0700

    # - Added "modify add,subtract,multiply,division" method

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        int a=5;
+        int b = 3;
         System.out.println("Hello world!");
-        int c = add (5, 6);
+        doAll(a,b);
+        doAll(10,5);
+        doAll(34,12);
+    }
+
+    public static void doAll(int a,int b) {
+        int c = add(a, b);
         System.out.println("c value is: " + c);
-        int d = subtract(10, 4);
+        int d = subtract(a, b);
         System.out.println("d value is: " + d);
+        int e = multiply(a, b);
+        System.out.println("e value is: " + e);
+        double f =division(a,b);
+        System.out.println("f value is:" + f);
+
     }
 
     public static int add(int a, int b) {
@@ -12,6 +25,15 @@ public class Main {
     }
 
     public static int subtract(int a, int b) {
-        return a -b;
+        return a - b;
+    }
+
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static float division(int a, int b) {
+        return (float)a / b;
     }
 }
