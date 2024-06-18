f0309106c483dfb86a85a4207589d5a80b47234c
Sudheer Donaboina <sdonaboina@caesars.com>
2024-06-18 13:21:27 -0700

    # - Added "subtract" method

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,15 @@ public class Main {
         System.out.println("Hello world!");
         int c = add (5, 6);
         System.out.println("c value is: " + c);
+        int d = subtract(10, 4);
+        System.out.println("d value is: " + d);
     }
 
     public static int add(int a, int b) {
         return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a -b;
     }
 }
